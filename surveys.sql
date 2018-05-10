141a6d44bd0e286066c6719fd19521c72d97150b
esteban legrand <esteban.legrand@outlook.fr>
2018-05-10 12:30:42 -0500

    Update database surveys.sql

--- surveys.sql
+++ surveys.sql
@@ -1,44 +1,163 @@
-ALTER TABLE Survey DROP CONSTRAINT FKSurvey300467;
-ALTER TABLE Project DROP CONSTRAINT FKProject159630;
-ALTER TABLE Users DROP CONSTRAINT FKUsers257902;
-ALTER TABLE Question DROP CONSTRAINT FKQuestion893077;
-ALTER TABLE Question DROP CONSTRAINT FKQuestion216667;
-ALTER TABLE QuestionOption DROP CONSTRAINT FKQuestionOp299459;
-ALTER TABLE QuestionOption DROP CONSTRAINT FKQuestionOp817468;
-ALTER TABLE Constraints DROP CONSTRAINT FKConstraint444998;
-DROP TABLE IF EXISTS Survey CASCADE;
-DROP TABLE IF EXISTS Project CASCADE;
-DROP TABLE IF EXISTS Users CASCADE;
-DROP TABLE IF EXISTS Profession CASCADE;
-DROP TABLE IF EXISTS Options CASCADE;
-DROP TABLE IF EXISTS TypeOfQuestion CASCADE;
-DROP TABLE IF EXISTS Question CASCADE;
-DROP TABLE IF EXISTS QuestionOption CASCADE;
-DROP TABLE IF EXISTS Constraints CASCADE;
-CREATE TABLE Survey (id SERIAL NOT NULL, Projectid SERIAL NOT NULL, name varchar(255) NOT NULL, description text, objective varchar(255), startDate date NOT NULL, finalDate date NOT NULL, DBname varchar(30) NOT NULL, PRIMARY KEY (id));
-CREATE TABLE Project (id SERIAL NOT NULL, description text NOT NULL, Useremail varchar(50) NOT NULL, name varchar(20), PRIMARY KEY (id));
-CREATE TABLE Users (email varchar(50) NOT NULL, idProfession SERIAL NOT NULL, name varchar(30) NOT NULL, lastName varchar(30) NOT NULL, password varchar(64) NOT NULL, birthDate date NOT NULL, nonce varchar(32), PRIMARY KEY (email));
+-- Database: "SGS"
+-- DROP DATABASE "SGS";
+CREATE DATABASE "SGS"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Spanish_Ecuador.1252'
+       LC_CTYPE = 'Spanish_Ecuador.1252'
+       CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE "SGS"
+  IS 'Survey Generator System';
+
+-- Table: public."Option"
+-- DROP TABLE public."Option";
+CREATE TABLE public."Option"
+(
+  id integer NOT NULL, -- Identification of the option
+  description character varying(80) NOT NULL, -- Name or description of the option
+  CONSTRAINT "Option_pkey" PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+
+-- Table: public."Question"
+-- DROP TABLE public."Question";
+CREATE TABLE public."Question"
+(
+  "idSurvey" integer NOT NULL, -- Identification of the survey
+  id integer NOT NULL, -- Identification (unique) of the question inside the survey (ordered)
+  title character varying(255) NOT NULL, -- The question itself
+  description character varying(255), -- The description of the question
+  "idType" character varying(5) NOT NULL, -- The identification of the type of the question
+  "order" integer NOT NULL, -- The order of the question in the survey
+  "shortName" character varying(20) NOT NULL, -- A short name for the question (to use in spreedsheets like tables)
+  CONSTRAINT "Question_pkey" PRIMARY KEY ("idSurvey", id),
+  CONSTRAINT "Question_idSurvey_fkey" FOREIGN KEY ("idSurvey")
+      REFERENCES public."Survey" (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE RESTRICT,
+  CONSTRAINT "Question_idType_fkey" FOREIGN KEY ("idType")
+      REFERENCES public."TypeOfQuestion" (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE RESTRICT,
+  CONSTRAINT "Question_order_check" CHECK ("order" > 0)
+)
+WITH (
+  OIDS=FALSE
+);
+
+-- Table: public."QuestionOption"
+-- DROP TABLE public."QuestionOption";
+CREATE TABLE public."QuestionOption"
+(
+  "idSurvey" integer NOT NULL, -- Identification of the survey
+  "idQuestion" integer NOT NULL, -- Identification of the question inside the survey
+  "idOption" integer NOT NULL, -- Identification of the option
+  "order" integer NOT NULL, -- The order of the option in the question
+  CONSTRAINT "QuestionOption_pkey" PRIMARY KEY ("idSurvey", "idQuestion", "idOption"),
+  CONSTRAINT "QuestionOption_idOption_fkey" FOREIGN KEY ("idOption")
+      REFERENCES public."Option" (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE RESTRICT,
+  CONSTRAINT "QuestionOption_idSurvey_fkey" FOREIGN KEY ("idSurvey", "idQuestion")
+      REFERENCES public."Question" ("idSurvey", id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE RESTRICT,
+  CONSTRAINT "QuestionOption_order_check" CHECK ("order" > 0)
+)
+WITH (
+  OIDS=FALSE
+);
+
 CREATE TABLE Profession (id SERIAL NOT NULL, description varchar(100) NOT NULL, PRIMARY KEY (id));
-CREATE TABLE Options (id SERIAL NOT NULL, description varchar(255) NOT NULL, PRIMARY KEY (id));
-CREATE TABLE TypeOfQuestion (id SERIAL NOT NULL, description varchar(20) NOT NULL, PRIMARY KEY (id));
-CREATE TABLE Question (id int4 NOT NULL, idSurvey int4 NOT NULL, idType SERIAL NOT NULL, title varchar(255) NOT NULL, description text, "order" int4, shortName varchar(20), PRIMARY KEY (id, idSurvey));
-CREATE TABLE QuestionOption (idOption SERIAL NOT NULL, idQuestion int4 NOT NULL, idSurvey int4 NOT NULL, "order" int4, PRIMARY KEY (idOption, idQuestion, idSurvey));
-CREATE TABLE Constraints (idQuestion int4 NOT NULL, idSurvey int4 NOT NULL, "constraint" varchar(50) NOT NULL, PRIMARY KEY (idQuestion, idSurvey));
-ALTER TABLE Survey ADD CONSTRAINT FKSurvey300467 FOREIGN KEY (Projectid) REFERENCES Project (id);
-ALTER TABLE Project ADD CONSTRAINT FKProject159630 FOREIGN KEY (Useremail) REFERENCES Users (email);
-ALTER TABLE Users ADD CONSTRAINT FKUsers257902 FOREIGN KEY (idProfession) REFERENCES Profession (id);
-ALTER TABLE Question ADD CONSTRAINT FKQuestion893077 FOREIGN KEY (idSurvey) REFERENCES Survey (id);
-ALTER TABLE Question ADD CONSTRAINT FKQuestion216667 FOREIGN KEY (idType) REFERENCES TypeOfQuestion (id);
-ALTER TABLE QuestionOption ADD CONSTRAINT FKQuestionOp299459 FOREIGN KEY (idOption) REFERENCES Options (id);
-ALTER TABLE QuestionOption ADD CONSTRAINT FKQuestionOp817468 FOREIGN KEY (idQuestion, idSurvey) REFERENCES Question (id, idSurvey);
-ALTER TABLE Constraints ADD CONSTRAINT FKConstraint444998 FOREIGN KEY (idQuestion, idSurvey) REFERENCES Question (id, idSurvey);
+-- Table: public."Profession"
+-- DROP TABLE public."Profession";
+CREATE TABLE public."Profession"
+(
+  "id" integer NOT NULL, -- Identification of the profession
+  "description" varchar(100) NOT NULL, -- Description de la profession
+  CONSTRAINT "Profession_pkey" PRIMARY KEY ("id"),
+  CONSTRAINT "User_idProfession_fkey" FOREIGN KEY ("idProfession")
+      REFERENCES public."Profession" (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE RESTRICT,
+)
+WITH (
+  OIDS=FALSE
+);
 
 
+-- Table: public."User"
+-- DROP TABLE public."User";
+CREATE TABLE public."User"
+(
+  "email" varchar(50) NOT NULL, -- Identification of the user
+  "idProfession" integer NOT NULL, -- Identification of the profession inside Profession
+  "name" varchar(30) NOT NULL, -- Name of the user
+  "lastName" varchar(30) NOT NULL, -- Last name of the user
+  "password" varchar(64) NOT NULL, -- Password of the user
+  "birthDate" date NOT NULL, -- birthdate
+  "nonce" varchar(32), -- nonce
+  CONSTRAINT "User_pkey" PRIMARY KEY ("email"),
+  CONSTRAINT "User_idProfession_fkey" FOREIGN KEY ("idProfession")
+      REFERENCES public."Profession" (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE RESTRICT,
+)
+WITH (
+  OIDS=FALSE
+);
 
+
+-- Table: public."Project"
+-- DROP TABLE public."Project";
+CREATE TABLE public."Project"
+(
+  "id" integer NOT NULL, -- Identification of the project
+  "description" varchar(255) NOT NULL, -- Description of the project
+  "userEmail" varchar(50) NOT NULL, -- Identification of the user email in User
+  "name" varchar(20) NOT NULL, -- The name of the project
+  CONSTRAINT "Project_pkey" PRIMARY KEY ("id"),
+)
+WITH (
+  OIDS=FALSE
+);
+
+
+-- Table: public."Survey"
+-- DROP TABLE public."Survey";
+CREATE TABLE public."Survey"
+(
+  "id" integer NOT NULL, -- Identification (unique) for a survey
+  "projectId" integer NOT NULL, -- Identification of a project inside Project
+  "name" character varying(80) NOT NULL, -- Name of the survey
+  "description" character varying(255), -- Description of the survey
+  "objective" character varying(255), -- Objetive(s) of the survey
+  "startDate" date, -- Starting date of the project
+  "finalDate" date, -- Final date of the project
+  "dbName" character varying(25) NOT NULL, -- The name of the database that will be created for the survey
+  CONSTRAINT "Survey_pkey" PRIMARY KEY (id)
+  CONSTRAINT "Survey_projectId_fkey" FOREIGN KEY ("projectId")
+      REFERENCES public."Project" (id) MATCH SIMPLE
+      ON UPDATE CASCADE ON DELETE RESTRICT,
+)
+)
+WITH (
+  OIDS=FALSE
+);
+
+- Table: public."TypeOfQuestion"
+
+-- DROP TABLE public."TypeOfQuestion";
+
+CREATE TABLE public."TypeOfQuestion"
+(
+  id character varying(5) NOT NULL, -- Identification of the type of question
+  description character varying(60) NOT NULL, -- Name or description of the type
+  CONSTRAINT "TypeOfQuestion_pkey" PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
 
 INSERT INTO Profession(description) VALUES ('Teacher');
-
-
 INSERT INTO TypeOfQuestion(description) VALUES ('Free');
 INSERT INTO TypeOfQuestion(description) VALUES ('Option');
 INSERT INTO TypeOfQuestion(description) VALUES ('Open Option');
